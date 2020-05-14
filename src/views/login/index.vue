8fe4c9c416a10571d71c9a71bee7f3a7df358d7b
dongpf <610841614@qq.com>
2020-05-14 23:06:04 +0800

    首页背景图更改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -193,8 +193,7 @@ $light_gray:#eee;
   min-height: 100%;
   width: 100%;
   background-color: $bg;
-  /*background-image: url("../../assets/bg/nuist40.jpg");*/
-  background-image: url("../../assets/bg/5.jpg");
+  background-image: url("../../assets/carousel/5.jpg");
   background-size: cover;
   overflow: hidden;
 
