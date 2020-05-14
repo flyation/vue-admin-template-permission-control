c15edb479c500a65a2bb6039e3a7b80271a12f71
dongpf <610841614@qq.com>
2020-05-14 22:33:58 +0800

    首页信息展示优化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,10 +1,56 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
-    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <el-row :gutter="40" class="panel-group">
+      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+        <div class="card-panel" @click="handleChangeRouter1">
+          <div class="card-panel-icon-wrapper icon-people">
+            <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+          </div>
+          <div class="card-panel-description">
+            <div class="card-panel-text">
+              预约记录
+            </div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+        <div class="card-panel" @click="handleChangeRouter2">
+          <div class="card-panel-icon-wrapper icon-message">
+            <svg-icon icon-class="shopping" class-name="card-panel-icon" />
+          </div>
+          <div class="card-panel-description">
+            <div class="card-panel-text">
+              预约座位
+            </div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :xs="12" :sm="12" :lg="7" class="card-panel-col">
+        <div>
+        </div>
+      </el-col>
+      <el-col :xs="12" :sm="12" :lg="5">
+        <el-card class="box-card">
+<!--          <div slot="header" class="clearfix">-->
+<!--            -->
+<!--          </div>-->
+          <div class="text item">
+            <b class="el-icon-user">&nbsp;{{ name }}</b>&nbsp;
+            <span v-for="role in roles" :key="role">
+              <el-tag type="primary" v-show="role === 'admin'">管理员</el-tag>
+              <el-tag type="success" v-show="role === 'teacher'">教师</el-tag>
+              <el-tag type="warning" v-show="role === 'editor'">学生</el-tag>
+            </span>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+<!--    <div class="dashboard-text">name: {{ name }}</div>-->
+<!--    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>-->
     <mallki class-name="mallki-text" :text="this.$store.state.user.avatar" />
 
-  <el-carousel :interval="2000" type="card" height="400px">
+  <el-carousel :interval="2000" type="card" height="370px">
     <el-carousel-item>
       <img src='../../assets/carousel/1.jpg' class="carousel_image_type">
     </el-carousel-item>
@@ -50,6 +96,14 @@ export default {
         '../../assets/carousel/3.jpg'
       ]
     }
+  },
+  methods: {
+    handleChangeRouter1() {
+      this.$router.push({ name: 'Record' })
+    },
+    handleChangeRouter2() {
+      this.$router.push({ name: 'Seat' })
+    }
   }
 }
 </script>
@@ -84,4 +138,115 @@ export default {
 .carousel_image_type{
   width: 100%;
 }
+</style>
+
+// panel group
+<style lang="scss" scoped>
+  .panel-group {
+    margin-top: 18px;
+
+    .card-panel-col {
+      margin-bottom: 32px;
+    }
+
+    .card-panel {
+      height: 108px;
+      cursor: pointer;
+      font-size: 12px;
+      position: relative;
+      overflow: hidden;
+      color: #666;
+      background: #fff;
+      box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
+      border-color: rgba(0, 0, 0, .05);
+
+      &:hover {
+        .card-panel-icon-wrapper {
+          color: #fff;
+        }
+
+        .icon-people {
+          background: #40c9c6;
+        }
+
+        .icon-message {
+          background: #36a3f7;
+        }
+
+        .icon-money {
+          background: #f4516c;
+        }
+
+        .icon-shopping {
+          background: #34bfa3
+        }
+      }
+
+      .icon-people {
+        color: #40c9c6;
+      }
+
+      .icon-message {
+        color: #36a3f7;
+      }
+
+      .icon-money {
+        color: #f4516c;
+      }
+
+      .icon-shopping {
+        color: #34bfa3
+      }
+
+      .card-panel-icon-wrapper {
+        float: left;
+        margin: 14px 0 0 14px;
+        padding: 16px;
+        transition: all 0.38s ease-out;
+        border-radius: 6px;
+      }
+
+      .card-panel-icon {
+        float: left;
+        font-size: 48px;
+      }
+
+      .card-panel-description {
+        float: right;
+        font-weight: bold;
+        margin: 26px;
+        margin-left: 0px;
+
+        .card-panel-text {
+          line-height: 18px;
+          color: rgba(0, 0, 0, 0.45);
+          font-size: 16px;
+          margin-bottom: 12px;
+        }
+
+        .card-panel-num {
+          font-size: 20px;
+        }
+      }
+    }
+  }
+
+  @media (max-width:550px) {
+    .card-panel-description {
+      display: none;
+    }
+
+    .card-panel-icon-wrapper {
+      float: none !important;
+      width: 100%;
+      height: 100%;
+      margin: 0 !important;
+
+      .svg-icon {
+        display: block;
+        margin: 14px auto !important;
+        float: none !important;
+      }
+    }
+  }
 </style>
